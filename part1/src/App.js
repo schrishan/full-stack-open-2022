aa40f466e2e5f527bde6c682f0011f7d606aa821
schrishan <schrishan.online@gmail.com>
2022-10-25 11:01:46 +0530

    1.13*: anecdotes step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -17,16 +17,24 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
+  const points = [1, 4, 6, 3, 9, 2, 5];
+  const copy = [...points];
    
   const [selected, setSelected] = useState(0);
 
-  const randomNumber = () => setSelected(Math.floor(Math.random() * (6 - 0 + 1) + 0));
-  
+  const randomNumber = () => {
+    setSelected(Math.floor(Math.random() * (6 - 0 + 1) + 0));
+  };
 
+  const addVote = () => {
+    copy[selected] += 1
+  }
+  
   return (
     <>
       <div>{anecdotes[selected]}</div>
-      <Button text="next anecdote" handleClick={randomNumber}/>
+      <div>has {copy[selected]} votes</div>
+      <Button text="vote" handleClick={addVote}/> <Button text="next anecdote" handleClick={randomNumber}/>
     </>
   )
 }
