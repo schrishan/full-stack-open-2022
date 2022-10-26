7b1024d07eb2681463553090de4f5631ef15bb88
schrishan <schrishan.online@gmail.com>
2022-10-26 12:09:36 +0530

    2.4: Course information step9

--- part1/src/Course.js
+++ part1/src/Course.js
@@ -5,27 +5,33 @@ const Header = ({ name }) => <h2>{name}</h2>
 const Part = ({ name, exercises }) => <div>{name} {exercises}</div>
 
 const Content = ({ parts }) => {
-   return (
-        parts.map(part => 
-                <Part key={part.id} name={part.name} exercises={part.exercises} />
+    return (
+        parts.map(part =>
+            <Part key={part.id} name={part.name} exercises={part.exercises} />
         )
     )
 }
 
-const Total = ({ total }) => <h4>total of {total} exercises</h4>
+const Total = ({ parts }) => {
+    const sum = parts.reduce((total, obj) => {
+        return total + obj.exercises;
+    }, 0);
+    return (
+        <h4>total of {sum} exercises</h4>
+    )
+}
+
+
+
 
 const Course = (props) => {
-    let t=0; 
-    const total = props.course.parts.reduce(function (s, p) {
-        t+=p.exercises
-        return t
-    })
     return (
-        <>
-            <Header name={props.course.name} />
-            <Content parts={props.course.parts} />
-            <Total total={total} />
-        </>
+        props.course.map((course, index) =>
+            <div key={index}>
+                <Header name={course.name} />
+                <Content parts={course.parts} />
+                <Total parts={course.parts} />
+            </div>)
     )
 }
 
