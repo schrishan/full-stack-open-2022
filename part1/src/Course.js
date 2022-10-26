fb2ad2fa89bd9781bf7d63bd2bdde25d5f6ec4a3
schrishan <schrishan.online@gmail.com>
2022-10-26 09:09:53 +0530

    2.2: Course information step7

--- part1/src/Course.js
+++ part1/src/Course.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-
 const Header = ({ name }) => <h2>{name}</h2>
 
 const Part = ({ name, exercises }) => <div>{name} {exercises}</div>
@@ -13,11 +12,18 @@ const Content = ({ parts }) => {
     )
 }
 
+const Total = ({ total }) => <h4>total of {total} exercises</h4>
+
 const Course = (props) => {
+    let total =0;
+    for(let i = 0 ; i < props.course.parts.length ; i++){
+        total += props.course.parts[i].exercises
+    }
     return (
         <>
             <Header name={props.course.name} />
             <Content parts={props.course.parts} />
+            <Total total={total} />
         </>
     )
 }
