f765718d8e41d0f62d4c61650471574c4ae5672f
schrishan <schrishan.online@gmail.com>
2022-11-10 08:47:17 +0530

    2.11: The Phonebook Step6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 
+import axios from 'axios';
+
 
 const App = () => {
-  const [persons, setPersons] =useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
-
+  const [persons, setPersons] =useState([]);
   const [filterName, setFilterName] = useState('');
   const [showAll, setShowAll] = useState(true);
 
-  const personsToShow = showAll
+    const hook = () => {
+      console.log('effect');
+      axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+    }
+    useEffect(hook, [])
+
+    const personsToShow = showAll
     ? persons
     : persons.filter(person => person.name === filterName);
 
