f766f32e52de5c65ce1381b94875516e2848f0c1
schrishan <schrishan.online@gmail.com>
2023-02-02 18:21:23 +0530

    3.1: Phonebook backend step1

---
+++ Part 3/backend/index.js
@@ -0,0 +1,114 @@
+const { request, response } = require('express')
+const express = require('express')
+const app = express()
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) =>{
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on post ${PORT}`)
+})
+
+
+// const { request, response } = require('express')
+// const express = require('express')
+// const app = express()
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
+
+// app.get('/', (request, response) =>{
+//   response.send('<h1>Hello World!</h1>')
+// })
+
+// app.get('/api/notes', (request, response) => {
+//   response.json(notes)
+// })
+
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const note = notes.find(note => note.id === id)
+//   note?response.json(note):response.status(404).end()
+// })
+
+// app.delete('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   notes = notes.filter(note => note.id !== id)
+
+//   response.status(204).end()
+// })
+
+// const PORT = 3001
+// app.listen(PORT, () => {
+//   console.log(`Server running on post ${PORT}`)
+// })
+
+// const http = require('http');
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
+
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'application/json'})
+//     response.end(JSON.stringify(notes));
+// });
+
+// const PORT=3001
+// app.listen(PORT)
+// console.log(`Server running on port ${PORT}`);
