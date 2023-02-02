1f536a51fbc6dad9f695b582100857f34c62e6a8
schrishan <schrishan.online@gmail.com>
2023-02-03 01:56:49 +0530

    3.5: Phonebook backend step5

--- Part 3/backend/index.js
+++ Part 3/backend/index.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 let persons = [
     { 
       "id": 1,
@@ -46,6 +47,32 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
