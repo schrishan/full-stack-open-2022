914d54aa4cd1aaad4335d9cac53aa20298d20f91
schrishan <schrishan.online@gmail.com>
2023-02-07 10:08:24 +0530

    3.9 phonebook backend step9

--- Part 3/backend/index.js
+++ Part 3/backend/index.js
@@ -77,9 +77,9 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on post ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 // const { request, response } = require('express')
--- part1/src/App.js
+++ part1/src/App.js
@@ -1,99 +1,150 @@
-import { useEffect, useState} from 'react'
-import axios from 'axios';
-import Weather from './components/Weather';
-
+import { useState, useEffect } from "react";
+import phonebookService from "./services/phonebook.service";
+import Footer from './components/Footer';
+import Notification from './components/Notification';
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [countries, setCountries] = useState([])
+  const [newContact, setNewContact] = useState({
+    id: null,
+    name: "",
+    number: "",
+  });
+  const [alertMessage, setAlertMessage] = useState({
+    message: "",
+    type: "",
+  });
+  const [phoneNumbers, setPhoneNumbers] = useState([]);
+  const [flterValue, setFlterValue] = useState("");
+  const [phoneNumbersToShow, setPhoneNumbersToShow] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      await axios
-        .get('https://restcountries.com/v3.1/all')
-        .then(response => {
-          setCountries(response.data)
-          setLoading(false);
-        }).catch(err => {
-          console.log('error', err);
-          setLoading(false);
+    phonebookService.getAll().then((initialPhoneNumbers) => {
+      setPhoneNumbers(initialPhoneNumbers);
+      setPhoneNumbersToShow(initialPhoneNumbers);
+    });
+  }, []);
+  const elOnChange = (e) => {
+    setNewContact((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const addNewNumber = (event) => {
+    event.preventDefault();
+    const contactFound = phoneNumbers.some(
+      (obj) => obj.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+    if (contactFound) {
+      if (
+        window.confirm(
+          `${newContact.name} already added to phonebook, replace the old number with a new one?`
+        ) == true
+      ) {
+        let contact = phoneNumbers.find((obj) => obj.name.toLowerCase() === newContact.name.toLowerCase())
+        const changedContact = { ...contact, number: newContact.number}
+
+          phonebookService
+          .update(changedContact.id, changedContact).then(returnedContact => {
+                    const contactIndex = phoneNumbers.findIndex(obj => {
+          return obj.name.toLowerCase() === newContact.name.toLowerCase();
         });
-    }
-    fetchData();
-  }, [])
-
-  console.log('countries', countries);
-
-  const [searchText, setSearchText] = useState("");
-
-  const filteredCountries = Object.values(countries).filter(
-    ({ name }) =>
-      name.common.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-  const showContent = (id) => {
-    let el = document.getElementById('id-'+id);
-    let btn = document.getElementById('btn-'+id);
-
-    if(el.classList.contains('show')) {
-      el.style.display = 'none';
-      el.classList.remove('show');
-      btn.innerHTML  = 'show';
+            phoneNumbers[contactIndex]['number'] = returnedContact.number;
+            setPhoneNumbers(phoneNumbers);
+            setNewContact({ id: null, name: "", number: "" });
+            setAlertMessage({message:`${newContact.name} number updated successfully.`, type:'success'})  
+            setTimeout(() => {
+              setAlertMessage({message:null, type:null});
+            }, 5000)
+          }).catch(error => {
+            setAlertMessage({message:`Note '${newContact.name}' was already removed from server`, type:'error'})
+            setTimeout(() => {
+              setAlertMessage(null)
+            }, 5000)
+          })
+      }
     } else{
-      el.classList.add('show');
-    el.style.display = 'block';
-    btn.innerHTML  = 'hide';
+      setNewContact((prevData) => ({
+        ...prevData,
+        id: phoneNumbers.length + 1,
+      }));
+      phonebookService.create(newContact).then((returnedContact) => {
+        setPhoneNumbers(phoneNumbers.concat(returnedContact));
+        setPhoneNumbersToShow(phoneNumbers.concat(returnedContact));
+        setNewContact({ id: null, name: "", number: "" });
+        setAlertMessage({message:`${newContact.name} added successfully.`, type:'success'})  
+            setTimeout(() => {
+              setAlertMessage({message:null, type:null});
+            }, 5000)
+      });
     }
-  }
-  console.log('filteredCountries', filteredCountries)
+  };
+
+  const deleteContact = (id, name, event) => {
+    if (window.confirm(`Delete ${name} ?`) == true) {
+      phonebookService.remove(id).then((returnedContacts) => {
+        setPhoneNumbers(phoneNumbers.filter((item) => item.id !== id));
+        setPhoneNumbersToShow(phoneNumbers.filter((item) => item.id !== id));
+      });
+    } else {
+      return false;
+    }
+  };
+
+  const handleSearch = (event) => {
+    setPhoneNumbersToShow(
+      phoneNumbers.filter((item) => {
+        return (
+          item.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !==-1
+        );
+      })
+    );
+  };
+
   return (
     <div>
-      {loading && <div>Loading...</div>}
-      <label>Find countries </label><input
-        type="text"
-        value={searchText}
-        onChange={({ target }) => setSearchText(target.value)}
-      />
-      <p> </p>
-      {searchText.length > 0 && filteredCountries.length >= 10 && <div>Too many matches, specify another filter.</div>}
-
-      {searchText.length > 0 && filteredCountries.length < 10 && filteredCountries.length > 1 && filteredCountries.map((country) => (
-        <>
-        <div key={country.name.common}>{country.name.common} <button id={`btn-${country.area}`} onClick={() => showContent(country.area)}>show</button>
-          <div  className="country-details" key={country.name.common} style={{display: "none"}} id={`id-${country.area}`}>
-            <h2>{country.name.common}</h2>
-            <p>capital {country.capital}
-              <br />area {country.area}</p>
-            <div><b>languages:</b></div>
-            <ul>
-              {Object.keys(country.languages).map(key => {
-                return <li>{country.languages[key]}</li>
-              })}
-            </ul>
-            <img src={country.flags.png} alt={country.name.common}/>
-              <Weather capital={country.capital}></Weather>
-          </div>
-        </div>
-        <br/>
-        </>
-      ))}
-
-      {searchText.length > 0 && filteredCountries.length === 1 && filteredCountries.map((country) => (
-        <div key={country.name.common}>
-          <h2>{country.name.common}</h2>
-          <p>capital {country.capital}<br />area {country.area}</p>
-          <div><b>languages:</b></div><br/>
-          <ul>
-            {Object.keys(country.languages).map(key => {
-              return <li>{country.languages[key]}</li>
-            })}
-          </ul>
-          <img src={country.flags.png}  alt={country.name.common}/>
-          <Weather capital={country.capital}></Weather>
-        </div>
-      ))}
+      <h2>Phonebook</h2>
+      <Notification message={alertMessage.message} type={alertMessage.type}/>
+      <br/>
+      <div className="fillter-wrp">
+        <label>filter showen with </label>
+        <input onChange={handleSearch} />
+      </div>
+      <br />
+      <form onSubmit={addNewNumber}>
+        <label>
+          name{" "}
+          <input name="name" value={newContact.name} onChange={elOnChange} />
+        </label>
+        <br />
+        <label>
+          number{" "}
+          <input
+            name="number"
+            value={newContact.number}
+            onChange={elOnChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Add</button>
+      </form>
+      <br />
+      <h2>Numbers</h2>
+      <ul>
+        {phoneNumbersToShow.map((number) => (
+          <li key={number.id}>
+            {number.name} {number.number}{" "}
+            <button
+              onClick={(event) => deleteContact(number.id, number.name, event)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <Footer/>
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
